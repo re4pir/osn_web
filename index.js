fb0f3ba52f14d6da175c681aece906e753597909
re4pir <andreybas04@gmail.com>
2022-11-29 08:33:19 +0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const btn = document.querySelector('.section-cost__button-text')
-const items = document.querySelectorAll('.section-test')
-const parent = document.querySelector('.section-promotions__context')
+const slider = document.querySelector('.section-cost__slider')
 
-btn.addEventListener('click', () => {
-    let elem = []
-    items.forEach(item => {
-        elem.push(item)
-    });
+const btns = document.querySelectorAll('section-cost__kits-kit')
+
+btns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if (btn.id === '1') {
+            slider.value = 0
+        } else if (btn.id === '2') {
+            slider.value = 50
+        } else {
+            slider.value = 100
+        }    
+    })
 })
